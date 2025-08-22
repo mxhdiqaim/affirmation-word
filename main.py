5b24858fdb389e054bde605435ad2d4635c2ea3b
mahdi <mxhdiqaim@gmail.com>
2025-08-22 22:45:14 +0100

    update affirmations list and enhance requirements for additional libraries

--- main.py
+++ main.py
@@ -5,16 +5,31 @@ app = Flask(__name__)
 
 # This is your list of affirmations. You can add as many as you like!
 affirmations = [
-    "You are my favorite person in the whole world.",
-    "Your smile makes my day brighter.",
-    "I am so proud of everything you do.",
+    "Your smile makes will my day brighter."
+    "You've got a great sense of humor."
+    "Your positive energy is contagious."
+    "I always have a good time with you."
     "I can't wait to see you again.",
     "You are beautiful, inside and out.",
     "Thinking of you always brings a smile to my face.",
     "You are strong and capable of amazing things.",
     "I am so lucky to have you in my life.",
     "Every moment with you is a gift.",
-    "You inspire me every day."
+    "It's always great to hear from you."
+    "You are smart and capable of amazing things."
+    "You have a great sense of style."
+    "It's always great to hear from you."
+    "You're such a thoughtful person."
+    "You have a killer sense of style.",
+    "You always know how to make me laugh.",
+    "I love hearing about what you're passionate about.",
+    "Your mind works in a really cool way.",
+    "You're a really good problem solver.",
+    "Your perspective on things is so unique.",
+    "It's always fun when we're hanging out.",
+    "You have a way of making people feel comfortable.",
+    "You're so dedicated to your goals.",
+    "You brighten up the whole room."
 ]
 
 # The main API endpoint that will return a random affirmation
